0396aadf3da0395a779c9bc401d2d8c0566aa118
Marko Justinek <marko.justinek@gmail.com>
2021-02-05 12:29:50 +1100

    Initial Commit

---
+++ PactMacOSExample/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PactMacOSExample
+//
+//  Created by Marko Justinek on 5/2/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ PactMacOSExample/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  PactMacOSExample
+//
+//  Created by Marko Justinek on 5/2/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
